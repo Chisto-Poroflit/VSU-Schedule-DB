cc4f593f1e0bf8044e35cfa603eb2e59bf98f3c8
LatyninG <39487750+LatyninG@users.noreply.github.com>
2019-09-22 19:37:17 +0300

    Lib

---
+++ SichCreateDB/DbBL/Models/Entity/CoupleGroup.cs
@@ -0,0 +1,12 @@
+﻿
+namespace SichCreateDB.Models.Entity
+{
+    public class CoupleGroup
+    {
+        public int CoupleId { get; set; }
+        public Couple Couple { get; set; }
+
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+    }
+}
---
+++ SichCreateDB/DbBL/Models/EntityConfig/TeacherConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SichCreateDB.Models.Entity;
+
+namespace SichCreateDB.Models.EntityConfig
+{
+    public class TeacherConfig: IEntityTypeConfiguration<Teacher>
+    {
+        public void Configure(EntityTypeBuilder<Teacher> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Id)
+                .ValueGeneratedOnAdd();
+            builder.Property(t => t.FullName).HasMaxLength(300);
+            builder.Property(t => t.Position).HasMaxLength(500);
+        }
+    }
+}
