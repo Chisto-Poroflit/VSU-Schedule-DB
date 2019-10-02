8c70ddc386ef7161c20611071d9dbfdf7f70a4d2
SteamDiver <steamdiver@yandex.ru>
2019-10-02 16:01:37 +0300

    data anotations

--- SichCreateDB/DbLibrary/Models/Entity/Room.cs
+++ SichCreateDB/DbLibrary/Models/Entity/Room.cs
@@ -1,18 +1,27 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace DbLibrary.Models.Entity
 {
     public class Room
     {
+        [Display(Name ="Номер")]
         public string Number { get; set; }
 
+        [Range(1, int.MaxValue, ErrorMessage = "Вместимость является целым положительным числом")]
+        [Display(Name ="Вместимость")]
         public int Capacity { get; set; }
 
+        [Display(Name = "Лекционная")]
         public bool IsLecture { get; set; }
 
+        [Display(Name = "Практическая")]
         public bool IsPractice { get; set; }
 
+        [Display(Name = "Лаборатория")]
         public bool IsLaboratory { get; set; }
 
+        [Display(Name = "Специальная")]
         public bool IsSpecial { get; set; }
 
         public Couple Couple { get; set; }
